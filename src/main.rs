c6dc4a04b6ec51bba66ad04b5c7d22349f918e4d
jordan <jordan.stachura@gmail.com>
2024-04-26 07:57:12 -0400

    minor

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 pub mod types;
+pub mod dummy;
 
 fn main() {
     println!("Hello, world!");
--- src/types.rs
+++ src/types.rs
@@ -1,5 +1,5 @@
 
-use std::net::{ IpAddr, SocketAddr };
+use std::net::SocketAddr;
 use tokio::net::UdpSocket;
 use std::collections::HashMap;
 use std::hash::Hash;
@@ -7,19 +7,31 @@ use serde::{ Serialize, Deserialize };
 
 
 /// Corresponds to the NeuronId from cajal. In the future, just re-export cajal's.
-#[derive(Serialize, Deserialize, Hash, Eq, PartialEq)]
+#[derive(Clone, Serialize, Deserialize, Hash, Eq, PartialEq)]
 pub struct NeuronId {
     structure: String,
     cluster: (usize, usize),
     neuron: (usize, usize)
 }
+impl std::fmt::Display for NeuronId {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}/{}:{}/{}:{}",
+            self.structure,
+            self.cluster.0,
+            self.cluster.1,
+            self.neuron.0,
+            self.neuron.1,
+        )
+    }
+}
+
 
 /// Sends some data as a NeuronId to trigger a Complex's Inputs.
 /// The frequency of that data's occurrence should form a meaningful signal.
 pub struct Sensor<T: Hash + Eq> {
-    pub(crate) address: SocketAddr,
-    pub(crate) socket: UdpSocket,
-    pub(crate) spectrum: HashMap<T, NeuronId>,
+    pub address: SocketAddr,
+    pub socket: UdpSocket,
+    pub spectrum: HashMap<T, NeuronId>,
 } impl<T: Hash + Eq> Sensor<T> {
 
     /// Create a sensor socket. Use port '0' to have the system assign a port.
@@ -41,9 +53,8 @@ pub struct Sensor<T: Hash + Eq> {
 
     /// Connect to a remote socket. Remember to ensure that the corresponding Input
     /// can handle all NeuronIds that will be sent by this sensor.
-    pub async fn connect(&self, remote: IpAddr) -> Result<(), anyhow::Error> {
-        let remote_socket = SocketAddr::new(remote, self.address.port());
-        self.socket.connect(remote_socket).await?;
+    pub async fn connect(&self, remote: &SocketAddr) -> Result<(), anyhow::Error> {
+        self.socket.connect(remote).await?;
         Ok(())
     }
 
@@ -58,12 +69,12 @@ pub struct Sensor<T: Hash + Eq> {
 }
 
 /// Handles the behavioral output of a bionic neural network made with cajal.
-/// When it receives a NeuronId, it executes the corresponding void function.
-pub struct Motor<B: Fn()> {
-    pub(crate) address: SocketAddr,
-    pub(crate) socket: UdpSocket,
-    pub(crate) nerves: HashMap<NeuronId, B>,
-} impl<B: Fn()> Motor<B> {
+/// When it receives a NeuronId, it executes the corresponding function.
+pub struct Motor {
+    pub address: SocketAddr,
+    pub socket: UdpSocket,
+    pub nerves: HashMap<NeuronId, ()>,
+} impl Motor {
 
     /// Create a motor socket. Use port '0' to have the system assign a port.
     /// The socket address will be recorded in the address field.
@@ -77,19 +88,17 @@ pub struct Motor<B: Fn()> {
         Ok(motor)
     }
 
-    /// Maps a neurotransmission signal to a function or closure to be executed.
-    pub fn add_nerve(&mut self, impulse: NeuronId, behavior: B) {
-        self.nerves.insert(impulse, behavior);
+    /// Maps a neurotransmission signal to a void function or closure to be executed.
+    pub fn add_nerve(&mut self, impulse: &NeuronId, behavior: ()) {
+        self.nerves.insert(impulse.clone(), behavior);
     }
 
     /// Receives NeuronId messages and executes the corresponding function or closure.
-    pub async fn recv_impulse(&self) -> Result<(), anyhow::Error> {
-        let mut buffer = vec![0; 100]; // TBD Avoid creating this, move to enclosing function? Or keep for consistency?
-        if let Ok(n_bytes) = self.socket.recv(&mut buffer).await {
-            // TODO: task spawn?
+    pub async fn recv_impulse(&self, buffer: &mut [u8]) -> Result<(), anyhow::Error> {
+        if let Ok(n_bytes) = self.socket.recv(buffer).await {
             let buff = &buffer[..n_bytes];
-            let behavior: NeuronId = bincode::deserialize_from(buff)?;
-            if let Some(behavior) = self.nerves.get(&behavior) { behavior() }
+            let impulse: NeuronId = bincode::deserialize_from(buff)?;
+            if let Some(behavior) = self.nerves.get(&impulse) { *behavior }
         }
         Ok(())
     }
