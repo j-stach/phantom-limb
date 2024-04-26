7e62a2f0abde30d7cf110b31c2b64fe0d777d0f8
jordan <jordan.stachura@gmail.com>
2024-04-26 03:59:51 -0400

    dummy

---
+++ src/dummy/motor.rs
@@ -0,0 +1,5 @@
+
+// Motor builder
+// - specify port to send to
+// - map neuron ids to behavioral action
+// - start with hello world to print ids
---
+++ src/dummy/sensor.rs
@@ -0,0 +1,6 @@
+
+// Sensor builder
+// - specify port to send to
+// - map stimuli to neuron ids
+// - start with randomly generating data
+// - then do one that reads std in
